a1771853c1a3b798b76f25fdafc792869f337b7e
MaxAsif <asifiqbal7.iitkgp@gmail.com>
2019-01-24 09:43:49 +0530

    added jwt login option

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 var express = require('express');
+var logger = require('pino')();
+var jwt = require('jsonwebtoken'),
 
+const middlewares = require('./middlewares/middleware');
 
 app = express();
 
-app.listen(3000,function(){
-  console.log('Server Started');
+app.listen(4000,function(){
+  logger.info('Server Started');
+});
+
+app.get('/api/login',function(req,res){
+  const user={
+    id:1,
+    username:'Asif',
+    password:'password123'
+  };
+  var token = jwt.sign({user},'sssshhhhh');
+  logger.info(token);
+  res.json(token);
 });
