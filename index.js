42ed6409fe52219148e15a5ed22297b08da798f9
MaxAsif <asifiqbal7.iitkgp@gmail.com>
2019-01-28 17:20:53 +0530

    added image download and resize option

--- index.js
+++ index.js
@@ -1,22 +1,49 @@
 var express = require('express');
 var logger = require('pino')();
-var jwt = require('jsonwebtoken'),
-
+var jwt = require('jsonwebtoken');
 const middlewares = require('./middlewares/middleware');
+var config = require('./config/key');
+var app = express();
 
-app = express();
+// PORT FOR OUR API
+// =============================================================================
+var port = process.env.PORT || 3000;
 
-app.listen(4000,function(){
-  logger.info('Server Started');
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();
+
+router.get('/', function(req, res) {
+    res.json({ message: 'welcome to our api!' });
 });
 
-app.get('/api/login',function(req,res){
+router.get('/login',function(req,res) {
   const user={
     id:1,
     username:'Asif',
     password:'password123'
   };
-  var token = jwt.sign({user},'sssshhhhh');
+  var token = jwt.sign({user},config.key);
   logger.info(token);
+  middlewares.localStorage.setItem("jwtoken",token);
+  console.log(middlewares.localStorage.length);
   res.json(token);
+});
+
+router.get('/image',function(req,res,next) {
+  middlewares.validity(req,res,next);
+  middlewares.ThumbnailCreation(req,res,next);
+  res.send('Image downloaded and compressed succesfully');
+});
+
+
+// REGISTER OUR ROUTES
+// ==============================================================================
+// all of our routes will be prefixed with /api
+app.use('/api', router);
+
+// START THE SERVER
+// =============================================================================
+app.listen(port,function(){
+  logger.info('Server Started at port : '+port);
 });
---
+++ middlewares/middleware.js
@@ -0,0 +1,64 @@
+var LocalStorage = require('node-localstorage').LocalStorage;
+var localStorage = new LocalStorage('./jwtoken');
+var jwt = require('jsonwebtoken');
+var download = require('image-downloader');
+var fs = require('fs');
+var resizeImg = require('resize-img');
+var path = require('path');
+var config = require('../config/key');
+
+const validity = function (req,res,next) {
+  // console.log(req.params);
+  let token = localStorage.getItem("jwtoken");
+  if(token){
+    jwt.verify(token,config.key,function (err,decoded) {
+      if(err){
+        res.render('unauthorized');
+      }
+      else {
+        next();
+      }
+    });
+  }
+  else{
+    console.log('unauthorized');
+  }
+};
+
+const ThumbnailCreation = (req,res,next)=>{
+  console.log('here');
+  let url = req.query.url;
+  // let url = 'https://drop.ndtv.com/albums/AUTO/mahindra-xuv300-exterior-and-interior/aeb38cee-aac7-436b-9bef-94a8dafa3734.jpg';
+  console.log(url);
+  let ext = path.extname(url);
+  console.log(ext);
+  if(ext === '.bmp' || ext === '.jpg' || ext === '.jpeg' || ext ==='.png'){
+    //generate
+    const options = {
+      url: url,
+      dest: './images'
+    };
+    download.image(options)
+    .then(({ filename, image }) => {
+      console.log(filename);
+      resizeImg(fs.readFileSync(filename), {width:50, height:50})
+      .then(img => {
+        console.log('working 2')
+        fs.writeFileSync("./thumbnails/"+filename, img);
+        next();
+      }).catch((err) => {
+        console.log(err);
+      });
+    }).catch((err) => {
+      console.log(err);
+    });
+  }
+  else{
+    res.send('File extensions allowed- [bmp,png,jpeg]');
+  }
+
+}
+
+module.exports.validity = validity;
+module.exports.localStorage=localStorage;
+module.exports.ThumbnailCreation = ThumbnailCreation;
